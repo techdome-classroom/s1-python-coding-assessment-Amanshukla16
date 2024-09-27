c9cc97885e85f7c2d5d6ce166103d3cb893701fa
Amanshukla16 <161189825+Amanshukla16@users.noreply.github.com>
2024-09-27 13:20:07 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,34 @@
 def decode_message( s: str, p: str) -> bool:
-
-# write your code here
   
-        return False
+  i = 0
+  j = 0
+
+  while i < len(message) and j < len(key):
+    if key[j] == '*':
+      # Skip any characters in the message until the next character in the key
+      while i < len(message) and j < len(key) and key[j] == '*':
+        i += 1
+      j += 1
+    elif key[j] == '?':
+      # Skip a single character in the message
+      i += 1
+      j += 1
+    elif message[i] == key[j]:
+      i += 1
+      j += 1
+    else:
+      return False
+
+  
+  return i == len(message) and j == len(key)
+  message1 = "aa"
+  key1 = "a"
+  print(secret_decoder_ring(message1, key1))  
+  message2 = "aa"
+  key2 = "*"
+  print(secret_decoder_ring(message2, key2))  
+
+  message3 = "cb"
+  key3 = "?a"
+  print(secret_decoder_ring(message3, key3))  
+  return False
